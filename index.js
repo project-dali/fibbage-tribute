5fe9972ce6087a394e3ff4b542372294d8c17e1e
Ben Schenerman <ben.schenerman@gmail.com>
2020-02-07 17:04:30 -0500

    ♻️ Refactor app to use our prompts

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
 // Import the Express module
-var express = require('express');
+const express = require('express');
 
 // Import the 'path' module (packaged with Node.js)
-var path = require('path');
+const path = require('path');
 
 // Create a new instance of Express
-var app = express();
+const app = express();
 
-// Import the Anagrammatix game file.
-var agx = require('./agxgame');
+// Import the thunk game file
+const thunk = require('./thunk');
+
+// Set Nunjucks
+app.set('view engine', 'nunjucks');
+const nunjucks = require('nunjucks');
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
+});
 
 // Create a simple Express application
-app.configure(function() {
-    // Turn down the logging activity
-    app.use(express.logger('dev'));
+app.configure(function () {
+	// Turn down the logging activity
+	app.use(express.logger('dev'));
 
-    // Serve static html, js, css, and image files from the 'public' directory
-    app.use(express.static(path.join(__dirname,'public')));
+	// Serve static html, js, css, and image files from the 'public' directory
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 // Create a Node.js based http server on port 8080
-var server = require('http').createServer(app).listen(process.env.PORT || 8080);
+const server = require('http').createServer(app).listen(process.env.PORT || 8080);
 
 // Create a Socket.IO server and attach it to the http server
-var io = require('socket.io').listen(server);
+const io = require('socket.io').listen(server);
 
 // Reduce the logging output of Socket.IO
-io.set('log level',1);
+io.set('log level', 1);
 
 // Listen for Socket.IO Connections. Once connected, start the game logic.
 io.sockets.on('connection', function (socket) {
-    //console.log('client connected');
-    agx.initGame(io, socket);
+	//console.log('client connected');
+	thunk.initGame(io, socket);
 });
 
 
